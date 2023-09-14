b396d561803f17a724d6ac211a232c4e92f4d813
kyaw1933 <145020069+kyaw1933@users.noreply.github.com>
2023-09-15 03:53:09 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ cyan='\e[96m'   #စိမ်းပြာရောင်
 none='\e[0m'    #အရောင်မရှိ
 
 #Username နှင့် Password ပြောင်းရန်
-username="myogyi"
+username="komyo"
 password="myo"
 
 #SSH USER LIMIT သတ်မှတ်ရန်
@@ -27,15 +27,15 @@ dias="2"
 # h6 သည် = စာလုံးအသေး ဖြစ်သည်
 # h4 သည် = စာလုံးအလတ် ဖြစ်သည်
 # h3 သည် = စာလုံးအကြီး  ဖြစ်သည်
-servermessage="<h3><font color='red'>
+servermessage="<h4><font color='red'>
 ▬▬▬▬▬▬▬▬✿4▪0▪4✿▬▬▬▬▬▬▬▬
-</font></h3>
+</font></h4>
 <h4><font color='cyan'>
 ↤↤↤↤↤ GCP SERVER BY ④ ⍤⃝✿④ ↦↦↦↦↦
 </font></h4>
-<h3><font color='red'>
+<h4><font color='red'>
 ▬▬▬▬▬▬▬▬✿4▪0▪4✿▬▬▬▬▬▬▬▬
-</font></h3>"
+</font></h4>"
 #End
 #ဒီအောက်က စာတွေပြင်ရန်မလိုပါ🌺
 [[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
